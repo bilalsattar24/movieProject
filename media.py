9106d2b2ce222cf437c2670ea66514ff06c3f2d1
Bilal Sattar <bilalsattar24@gmail.com>
2016-10-24 17:11:04 -0700

    added documentation for movie class

--- media.py
+++ media.py
@@ -1,3 +1,21 @@
+"""
+Author - Bilal Sattar
+
+This class represents a movie object.
+
+Instance variables:
+
+title - title of movie
+storyline - a short description of the movie
+poster_image_url - location of image used for boxart of the movie
+trailer_youtube_url - URL of youtube movie trailer
+
+Methods:
+__init__ - Constructs the movie object with arguments provided
+
+"""
+
+#construct
 class Movie():
     def __init__(self, movieTitle, movieStory, movieImage, movieTrailer):
         self.title = movieTitle
